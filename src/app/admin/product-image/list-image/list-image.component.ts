0e7cf41ec049af0032d13193138a3200a8cfdeef
nhunguyendinh1902 <nhunguyendinh75@gmail.com>
2023-10-20 21:54:53 +0700

    nhund commit

--- src/app/admin/product-image/list-image/list-image.component.ts
+++ src/app/admin/product-image/list-image/list-image.component.ts
@@ -1,10 +1,40 @@
 import { Component } from '@angular/core';
-
+import { ProductImageService } from '../product-image.service';
+import { ActivatedRoute, Router } from '@angular/router';
 @Component({
   selector: 'app-list-image',
   templateUrl: './list-image.component.html',
   styleUrls: ['./list-image.component.css']
 })
 export class ListImageComponent {
+  listImage!: any;
+  FileImage!: File;
+  Id!: number
+  constructor(
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+    private pis: ProductImageService,
+  ) { }
+  ngOnInit(): void {
+    this.activatedRoute.paramMap.subscribe(query => {
+      console.log(query.get("productId"));   // lay id
+      let id = query.get("productId");
+      this.pis.getListImage(id).subscribe(res => {
+        this.listImage = res.list;
+      });
+  })
+  }
 
+  onChange(event: any) {
+    this.FileImage = event.target.files;
+  }
+
+  onSubmit(): void {
+    this.activatedRoute.paramMap.subscribe(query => {
+      let id = query.get("productId");
+      var addForm = new FormData();
+      // addForm.append("IdProduct");
+      this.pis.addProductImage(addForm)
+    })
+  }
 }
